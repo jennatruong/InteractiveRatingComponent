63be9c696052d50b5d8f5bf81bd9e78b2505429f
Jenna Truong <jktruong@spscommerce.com>
2022-04-20 10:17:26 -0500

    initial pages

---
+++ docs/src/components/App/App.js
@@ -0,0 +1,28 @@
+import React, { useContext, useState} from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import GlobalState from '../../context/GlobalState'
+import Page1 from '../Page1/Page1'
+
+const App = () => {
+    
+
+    return(
+
+        <GlobalState>
+            <BrowserRouter>
+                <Switch>
+                    <Route exact path="/">
+                        <Page1 />
+                    </Route>
+                    <Route path="/page1">
+                        <Page1 />
+                    </Route>
+                </Switch>
+            </BrowserRouter>
+        </GlobalState>
+
+    );
+
+}
+
+export default App
