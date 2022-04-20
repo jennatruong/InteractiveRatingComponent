de826831f9829f7dc90a13011b129c2ba1ebd91e
Jenna Truong <jktruong@spscommerce.com>
2022-04-20 10:29:28 -0500

    display pages?

---
+++ docs/main.js
@@ -0,0 +1,11 @@
+import { StrictMode } from 'react';
+import * as ReactDOM from 'react-dom';
+
+import App from '../docs/src/components/App/App';
+
+ReactDOM.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  document.getElementById('root')
+);
